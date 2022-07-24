aff87502ebe9f3172d81f9ffd8858af4d5482e51
sergereinov <serge.reinov@gmail.com>
2022-07-24 17:55:11 +0300

    fix time convertion

--- internal/pg/tasks.go
+++ internal/pg/tasks.go
@@ -2,9 +2,10 @@ package pg
 
 import (
 	"database/sql"
+	"time"
+
 	"github.com/sirupsen/logrus"
 	"gitlab.com/g6834/team41/tasks/internal/models"
-	"time"
 )
 
 type Tasks struct {
@@ -60,9 +61,12 @@ func (t Tasks) GetAllTasksByEmail(email string) ([]models.Task, error) {
 
 	for rows.Next() {
 		var task models.Task
-		if err := rows.Scan(&task.ID, &task.Name, &task.Description, &task.CreatorEmail, &task.CreatedAt, &task.FinishedAt); err != nil {
+		var createdAt, finishedAt int64
+		if err := rows.Scan(&task.ID, &task.Name, &task.Description, &task.CreatorEmail, &createdAt, &finishedAt); err != nil {
 			return nil, err
 		}
+		task.CreatedAt = time.Unix(createdAt, 0)
+		task.FinishedAt = time.Unix(finishedAt, 0)
 		tasks = append(tasks, task)
 	}
 
